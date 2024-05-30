94c38f57161741d1dc37589052c93557e3911fe7
Rohit kapadnis <kapadnisrohit03@gmail.com>
2024-05-30 20:39:21 +0530

    Day 5 coding - deleted code because of issues

---
+++ Plan/plan.md
@@ -0,0 +1,12 @@
+<body>
+    <Header>
+    Logo,       separate div    Logo and Shop Same parent div
+    Shop,       separate div    Logo and Shop Same parent div
+    navbar,     separate div,   all navigation in one div
+    login,      separate div And Login Cart Hamburger same parent div
+    cart,       separate div And Login Cart Hamburger same parent div
+    hamburger   separate div And Login Cart Hamburger same parent div
+    </Header>
+    
+
+</body>
--- script.js
+++ script.js
@@ -6,18 +6,25 @@ let cart = document.getElementsByClassName("cartBTN");
 // Animation for first image of the website's Home Page
 const imageContainer = document.querySelector('.image-one img');
 gsap.from(imageContainer, {
-  scale: 2,
+  scale: 1.7,
   y: -50,
   scrollTrigger: {
     trigger: ".image-one img",
     start: "top bottom",
     end: "bottom top",
-
     scrub: true,
   }
 })
 
 
+// let hamburger = getElementsByClassName('ri-menu-line')
+// let shop = document.getElementsByClassName('shop')
+// let navLi = document.getElementsByClassName('navLi')
+
+// hamburger.addEventListener('click', ()=>{
+//   shop.classList.toggle('shop')
+//   navLi.classList.toggle('navLi')
+// })
 
 
 
